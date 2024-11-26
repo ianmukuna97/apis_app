da2a3221ad97a520f6bcebb2e2432cb20a879f2a
ianmukuna97 <ianmukuna97@gmail.com>
2024-11-26 14:40:02 +0300

    Django Rest framework

--- apis/my_serializers.py
+++ apis/my_serializers.py
@@ -1,4 +1,10 @@
 from rest_framework import serializers
 
+from apis.models import Student
+
 
 class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at', 'id']
--- apis/views.py
+++ apis/views.py
@@ -1,29 +1,53 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from http import HTTPStatus
 
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 from apis.models import Student
+from apis.my_serializers import StudentSerializer
 
 
 # Create your views here.
 @api_view(['GET'])
 def students_list(request):
     students = Student.objects.all()
-    return None
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def single_student(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    serializer = StudentSerializer(student)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_student(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    serializer = StudentSerializer(student, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_student(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    student.delete()
+    return Response(status=HTTPStatus.NO_CONTENT)
 
 
-def single_student(request):
-    return None
-
-
-def update_student(request):
-    return None
-
-
-def delete_student(request):
-    return None
-
-
+@api_view(['POST'])
 def create_student(request):
-    return None
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=HTTPStatus.CREATED)
+    return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
 
-# Serializers are forms
+# 200-- OK - 300 - Redirect - 400- bad,  500- bad on server
+
+# class based views
+# filters
+# validations
+
+# django rest tutorials
